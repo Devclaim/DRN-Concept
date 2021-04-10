e2fc2aa8d52bbc637398a8042c971037faf2910a
Devclaim <Devclaim@users.noreply.github.com>
2021-04-10 16:13:05 +0200

    +three.js first implementation

--- DRNwebsite/src/app/app.component.ts
+++ DRNwebsite/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import * as THREE from 'three';
+
+
 
 
 @Component({
@@ -6,6 +9,40 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'mut-website';
+  @ViewChild('rendererContainer') rendererContainer: ElementRef;
+
+  renderer = new THREE.WebGLRenderer();
+  scene = null;
+  camera = null;
+  mesh = null;
+
+  constructor() {
+  
+      this.scene = new THREE.Scene();
+
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+      this.camera.position.z = 1000;
+
+      const geometry = new THREE.BoxGeometry(200, 200, 200);
+      const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+      this.mesh = new THREE.Mesh(geometry, material);
+
+      this.scene.add(this.mesh);
+  }
+
+  ngAfterViewInit() {
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+      this.animate();
+  }
+
+  animate() {
+      window.requestAnimationFrame(() => this.animate());
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.y += 0.02;
+      this.renderer.render(this.scene, this.camera);
+  }    
 }
